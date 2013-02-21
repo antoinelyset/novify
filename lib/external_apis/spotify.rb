b1e802944f4e40d5ffb36a650977aae73bab9c5c
Antoine Lyset <antoinelyset+github@gmail.com>
2013-02-21 22:15:30 +0100

    Refactor spotify

--- lib/external_apis/spotify.rb
+++ lib/external_apis/spotify.rb
@@ -18,11 +18,16 @@ module ExternalApis
           Thread.current[:spotify_track] = MetaSpotify::Track.search("#{t.formatted_name} #{t.formatted_artist}")[:tracks].first
         end
       end
+      extract_tracks(threads)
+      @tracks
+    end
+
+    def extract_tracks(threads)
       threads.each_with_index do |thread, i|
         thread.join
         spotify_track = thread[:spotify_track]
         # TODO : Don't next, relaunch it with just the title,
-        #  then fuzzy match on each artist keep the highest
+        #   fuzzy match on each artist, then keep the highest
         next if spotify_track.nil?
         @tracks[i].attributes = {
             spotify_name: spotify_track.name,
